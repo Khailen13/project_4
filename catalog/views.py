5e604343c482f4904a256ad26cba9adb992edfe8
Lenar Khaidarov <haidarov_lenar@mail.ru>
2025-11-12 20:08:27 +0300

    init django project

---
+++ catalog/views.py
@@ -0,0 +1,11 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+def home(request):
+    return render(request, 'home.html')
+
+def contacts(request):
+    if request.method == "POST":
+        name = request.POST.get("name")
+        return HttpResponse(f"Спасибо, {name}! Сообщение получено.")
+    return render(request, 'contacts.html')
